ec6f216c96d36e685fdceb2d3e9c04a2d01e575b
Charles Ewel and Kevin VanEvery <charles@>
2016-06-14 09:42:50 -0700

    add html output for movie schedule

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*This file is for your custom js.  All yours*/
 
 //Business Logic
-function Movie(name, time, 3D) {
+function Movie(name, time, threeD) {
   this.name = name;
   this.time= time;
   this.threeD = threeD;
@@ -33,12 +33,25 @@ Ticket.prototype.price = function () {
   }
 }
 
+Movie.prototype.className = function () {
+  var movieClassName = this.name.replace(/\W/g, "");
+  return movieClassName;
+}
+
 var StarWars = new Movie("Star Wars", [1300,1600,2000],true)
 var Casablanca = new Movie("Casablanca", [1400,1730,2100],false)
+var movieArray = [StarWars, Casablanca];
+
 //Front-End Logic
 // Calls input from form-input.html
 
 $(document).ready(function(){
+  movieArray.forEach(function (element) {
+    $(".movie-schedule").append('<h2>' + element.name + '</h2>' + '<ul class = "movie-times ' + element.className() + '">' + '</ul>')
+    element.time.forEach(function (show) {
+      $(".movie-schedule ul." + element.className() ).append('<li>' + show + '</li>');
+    })
+  })
 
   $("#input").submit(function(event){
     event.preventDefault();
