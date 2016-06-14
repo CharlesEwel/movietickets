85ab86f801bbd7d66b01ee7b0e0b2c2317e32a74
Charles Ewel and Kevin VanEvery <kevin@>
2016-06-14 10:42:55 -0700

    create front end javascript controls for form input

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,33 @@ function Movie(name, time, threeD) {
   this.threeD = threeD;
 }
 
-function Ticket(age, movie, discounts) {
+function Ticket(age, time, threeD, discounts) {
   this.age = age;
-  this.movie = movie;
-  this.discounts= time;
+  this.time = time;
+  this.threeD = threeD;
+  this.discounts= discounts;
 }
 
 Ticket.prototype.price = function () {
   var cost = 12;
-  if (ticket.movie.time<1700){
+  debugger
+  if (this.time<1700){
     cost-=3;
   }
-  if (ticket.age>=62){
+  if (this.age>=62){
     cost-=2;
-  } else if(ticket.discounts[0]===true) {
+  } else if(this.discounts[0]==="military") {
     cost-=2;
-  } else if(ticket.discounts[1]===true) {
+  } else if(this.discounts[1]==="student") {
     cost -=1;
   }
-  if(ticket.movie.threeD===true) {
+  if(this.threeD===true) {
     cost+=3;
   }
-  if(ticket.age<2) {
+  if(this.age<2) {
     cost=0;
   }
+  return cost;
 }
 
 Movie.prototype.className = function () {
@@ -45,21 +48,43 @@ var movieArray = [StarWars, Casablanca];
 //Front-End Logic
 // Calls input from form-input.html
 
+var threeD=false;
+var time=1700;
+
 $(document).ready(function(){
   movieArray.forEach(function (element) {
     $(".movie-schedule").append('<h2>' + element.name + '</h2>' + '<ul class = "movie-times ' + element.className() + '">' + '</ul>')
-    element.time.forEach(function (show) {
-      $(".movie-schedule ul." + element.className() ).append('<li>' + show + '</li>');
-    })
+    if (element.threeD) {
+      $(".movie-schedule h2").append("<span class = 'threed'> 3D</span>");
+      element.time.forEach(function (show) {
+        $(".movie-schedule ul." + element.className() ).append('<li><span class="show3d">' + show + '</span></li>');
+      });
+    } else {
+      element.time.forEach(function (show) {
+        $(".movie-schedule ul." + element.className() ).append('<li><span class="show">' + show + '</span></li>');
+      });
+    }
+
   })
 
-  $("#input").submit(function(event){
+  $("#new-ticket").submit(function(event){
     event.preventDefault();
-    var input = ($("#blank").val());
 
-    var output = "";
+    var age = parseInt($("#age").val());
+    var discounts=[$("#military:checked").val(),$("#student:checked").val()]
+    var newTicket=new Ticket(age, time, threeD, discounts);
+    debugger;
+    $('#output').text(newTicket.price());
 
-    $('#output').text(output);
+  });
+
+  $(".show3d").click(function(){
+    threeD=true;
+    time = this.innerHTML;
+  });
+  $(".show").click(function(){
+    threeD=false;
+    time = this.innerHTML;
 
   });
 });
